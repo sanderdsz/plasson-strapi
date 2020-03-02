a928496104d52f6b2931bcd4598287376e3f6afb
sanderdsz <sanderdsz@gmail.com>
2020-03-01 21:13:57 -0300

    build for heroku

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(routes);
 
 // Server static assets producao
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static('frontend/build'));
 
 	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	);
 }
 

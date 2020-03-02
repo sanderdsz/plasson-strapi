0e85a0a6ded718b7f34c2315c2bc626edcc00141
sanderdsz <sanderdsz@gmail.com>
2020-03-01 21:07:36 -0300

    heroku structure

--- server.js
+++ server.js
@@ -3,16 +3,30 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-const server = express();
+const app = express();
 
 mongoose.connect(
 	'mongodb+srv://sanderdsz:admin@cluster0-ahwa4.mongodb.net/tindev?retryWrites=true&w=majority',
 	{ useUnifiedTopology: true }
 );
 
-server.use(cors());
+app.use(cors());
 
-server.use(express.json());
-server.use(routes);
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(routes);
 
-server.listen(3333);
+// Server static assets producao
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
+
+// Objeto procura PORTA do ambiente
+const PORT = process.env.PORT || 5000;
+
+// npm run server => Para iniciar o servidor
+app.listen(PORT, () => console.log(`Servidor iniciou na porta ${PORT}`));
